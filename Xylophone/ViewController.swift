e35ec2bd4bb2660b652ffe8b0568e62819c81d08
nibim <nibm@kth.se>
2025-04-04 21:57:15 +0200

    Feature: Animating the fading of buttoms for 0.2 seconds with alpha properties

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -11,16 +11,26 @@ import AVFoundation
 
 class ViewController: UIViewController {
     var player: AVAudioPlayer?
-
+    var duration: TimeInterval = 0.3
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
     @IBAction func KeyPressed(_ sender: UIButton) {
-        
+        print(sender.alpha)
+        changeOpacity(for: sender, duration: 0.5)
         playSound(soundName: sender.currentTitle!)
     }
-    
+    func changeOpacity(for button: UIButton, duration: TimeInterval) {
+        UIView.animate(withDuration: duration, animations: {
+            button.alpha = 0.5  // Fade to 50%
+        }) { _ in
+            UIView.animate(withDuration: duration) {
+                button.alpha = 1.0  // Fade back to normal after animation completes
+            }
+        }
+    }
+
     
     func playSound(soundName : String) {
         guard let path = Bundle.main.url(forResource: soundName, withExtension: "wav") else {
