7a2ae62bc41975cf356e4239759185c198a903ea
nibim <nibm@kth.se>
2025-04-04 11:49:21 +0200

    Feature: connect the C button to audio C.wav using AVfoundation library

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,31 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    var player: AVAudioPlayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func KeyPressed(_ sender: UIButton) {
+        playSound()
+    }
     
+    
+    func playSound() {
+        guard let path = Bundle.main.url(forResource: "C", withExtension: "wav") else {
+            return }
+        let url = URL(fileURLWithPath: path.path)
 
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player?.play()
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
 }
 
